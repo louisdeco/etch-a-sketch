d60146567759621a3044afd4b7d86e0b509c1300
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-19 09:42:15 +0400

    Modify rainbow modified
    Allow the user to switch freely between rainbow
    mode and normal mode thanks to the switch button.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Get the color
 let color;
 
-// Updtade the color
+// Update the color
 const colorSelector = document.querySelector("#color");
 colorSelector.addEventListener("input", updateFirst);
 
 function updateFirst(e) {
     color = e.target.value;
-}
+};
 
 // Create the canvas
 const buttonSizeSelector = document.querySelector("#btn");
@@ -29,7 +29,7 @@ function createPixels(size) {
         let pixel = document.createElement("div");
         pixel.setAttribute("style", `width: ${sizeOfPixel}%; height: ${sizeOfPixel}; background-color: white`);
         pixel.classList.add("pixel");
-        pixel.addEventListener("mouseover", colorHovering)
+        pixel.addEventListener("mouseover", colorHovering);
         canvas.appendChild(pixel);
     };
 };
@@ -44,4 +44,31 @@ function removeAllChildNodes() {
 // Hovering effect
 function colorHovering(e) {
     e.target.style.backgroundColor = color;
+}
+
+
+// Rainbow mode
+const rainbowSelector = document.querySelector("#check");
+rainbowSelector.addEventListener("change", () => {
+    const pixels = document.querySelectorAll(".pixel");
+    if (rainbowSelector.checked) {
+        pixels.forEach((pixel) => {
+            pixel.addEventListener("mouseover", randomColor);
+        });
+    }
+    else {
+        pixels.forEach((pixel) => {
+            pixel.removeEventListener("mouseover", randomColor);
+        });        
+        color = colorSelector.value;
+    }
+
+});
+
+// Random color
+function randomColor(e) {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    e.target.style.backgroundColor = "rgb("+red+", "+green+", "+blue+")";
 }
